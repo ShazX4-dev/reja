12f66f7fc0e100231a8dfe70a0cfb74b028e8759
ShazX4-dev <bratva.shah88@gmail.com>
2026-04-27 23:25:40 +0900

    fix: push qilishga urinish

--- app.js
+++ app.js
@@ -31,7 +31,15 @@ app.set("view engine", "ejs");
 // 4: Routing code
 app.post("/create-item", (req, res) => {
   console.log(req.body);
-  res.json({ test: "success" });
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.end("successfully added");
+    }
+  });
 });
 
 app.get("/author", (req, res) => {
