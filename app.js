5dc01e2f2037b5f286c165c9467eaf4b78c12f6d
ShazX4-dev <bratva.shah88@gmail.com>
2026-05-01 22:15:38 +0900

    feat: frontend javascripni qurish

--- app.js
+++ app.js
@@ -28,11 +28,8 @@ app.post("/create-item", (req, res) => {
   console.log("user entered /create-item");
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log("somethin went wrong");
-    } else {
-      res.end("successfully added");
-    }
+    console.log(data.ops);
+    res.json(data.ops[0]);
   });
 });
 
